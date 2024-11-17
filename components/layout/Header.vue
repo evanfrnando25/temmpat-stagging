6349bb10a04c691b07d9649f3cc89ad68fa0d700
evanfrnando25 <evanfrnando@gmail.com>
2024-11-17 18:07:21 +0700

    change logo directory

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -2,7 +2,7 @@
     <div class="header">
         <div class="header__wrapper">
             <div class="header__logo">
-                <img src="@/assets/img/logo.svg" />
+                <img src="/logo.svg" />
             </div>
             <div class="header__search">
                 <CommonInputSearch v-model="search" @focus="onFocus" @blur="onBlur" />
